48d3d60b167d4fcc03ef4923f0eaaeef4361e299
Yinon <68754714+yinon4@users.noreply.github.com>
2021-05-06 16:20:54 +0300

    Added print balance func

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 import blockchain
 from hashlib import sha256
 
+def printAddressBalance(address):
+    print(f"Balance of address {address}: {YCoin.getBalanceOfAddress(address)}")
+
 def set_balance(address, amount):
     YCoin.addTransaction(blockchain.Transaction(None, address, amount))
     YCoin.minePendingTransactions(None)
 
 if __name__ == '__main__':
     YCoin = blockchain.Blockchain()
-    set_balance("1", 100)
-    YCoin.addTransaction(blockchain.Transaction("1", "2", 50))
-    YCoin.addTransaction(blockchain.Transaction("2", "3", 50))
+    set_balance('A1', 100)
+    YCoin.addTransaction(blockchain.Transaction('A1', 'A2', 50))
+    YCoin.addTransaction(blockchain.Transaction('A2', 'A3', 50))
 
-    YCoin.minePendingTransactions("2")
+    YCoin.minePendingTransactions('A2')
     YCoin.minePendingTransactions(None)
 
-    print("Balance of address: " + str(YCoin.getBalanceOfAddress("1")))
-    print("Balance of address: " + str(YCoin.getBalanceOfAddress("2")))
-    print("Balance of address: " + str(YCoin.getBalanceOfAddress("3")))
+    printAddressBalance('A1')
+    printAddressBalance('A2')
+    printAddressBalance('A3')
 
 
     print("Chain Valid") if YCoin.isChainValid() else print("Chain Unvalid")
