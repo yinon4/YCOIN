d9a8ad9bedd1a927d6a7a8fbd3ceb19b9fa40c60
Yinon <68754714+yinon4@users.noreply.github.com>
2021-05-07 18:13:26 +0300

    added some functions for ease of use

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import blockchain
 from hashlib import sha256
 
+def printChainHistory(chain):
+    print("\nChain History:\n" + str(chain))
+
+def printChainValidity(chain):
+    print("Chain Valid!") if chain.isChainValid() else print(f"Chain Unvalid!")
+
+def mineBlock(address = None):
+    YCoin.minePendingTransactions(address)
+
 def printAddressBalance(address):
     print(f"Balance of address {address}: {YCoin.getBalanceOfAddress(address)}")
 
@@ -14,13 +23,11 @@ if __name__ == '__main__':
     YCoin.addTransaction(blockchain.Transaction('A1', 'A2', 50))
     YCoin.addTransaction(blockchain.Transaction('A2', 'A3', 50))
 
-    YCoin.minePendingTransactions('A2')
-    YCoin.minePendingTransactions(None)
-
+    mineBlock('A2')
+    mineBlock()
     printAddressBalance('A1')
     printAddressBalance('A2')
     printAddressBalance('A3')
 
-
-    print("Chain Valid") if YCoin.isChainValid() else print("Chain Unvalid")
-    print("\nChain History:\n" + str(YCoin))
+    printChainValidity(YCoin)
+    printChainHistory(YCoin)
